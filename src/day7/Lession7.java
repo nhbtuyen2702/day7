7b60c47ac4f85a0659e368ae3e5f5b46e1a94119
TuyenNguyen <nhbtuyen2702@gmail.com>
2022-03-15 19:13:18 +0700

    change file Practice7.java and Lession7.java

--- src/day7/Lession7.java
+++ src/day7/Lession7.java
@@ -3,7 +3,7 @@ package day7;
 public class Lession7 {
 
 	public static void main(String[] args) {
-		System.out.println("Lession 7");
+		System.out.println("Lession 77");
 	}
 
 }
--- src/day7/Practice7.java
+++ src/day7/Practice7.java
@@ -3,7 +3,7 @@ package day7;
 public class Practice7 {
 
 	public static void main(String[] args) {
-		System.out.println("Practice7");
+		System.out.println("Practice77");
 	}
 
 }
